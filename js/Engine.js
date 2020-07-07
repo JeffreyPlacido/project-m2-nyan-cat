7f563ad9c109db3a352f693e8201c4bfc8736609
Jeffrey <jeffreyplacido@hotmail.com>
2020-07-07 11:02:10 -0400

    modified collision

--- js/Engine.js
+++ js/Engine.js
@@ -59,10 +59,32 @@ class Engine {
   };
 
   isPlayerDead = () => {
-    return this.enemies.some((ship) => {
-      return ship.y + ENEMY_HEIGHT > gameEngine.player.y && ship.x === gameEngine.player.x;
-    });
-  };
+      for (let i = 0; i < this.enemies.length; i++) {
+        let playerLeft = this.player.x;
+        let playerRight = this.player.x + PLAYER_WIDTH;
+        let playerTop = GAME_HEIGHT - PLAYER_HEIGHT +10;
+        let playerBot = GAME_HEIGHT - 10;
+        let enemyLeft = this.enemies[i].x;
+        let enemyRight = this.enemies[i].x + ENEMY_WIDTH;
+        let enemyTop = this.enemies[i].y;
+        let enemyBot = this.enemies[i].y + ENEMY_HEIGHT;
+        console.log("playerLeft < enemyRight", playerLeft, enemyRight);
+        console.log("playerRight > enemyLeft", playerRight, enemyLeft);
+        console.log("playerTop < enemyBot", playerTop, enemyBot);
+        console.log("playerBot > enemyTop", playerBot, enemyTop);
+        if (
+          playerLeft < enemyRight &&
+          playerRight > enemyLeft &&
+          playerTop < enemyBot &&
+          playerBot > enemyTop
+        ) {
+          console.log("hit");
+          return true;
+        }
+      }
+      return false;
+    };
+
 
   getPoints = () => {
     return this.hearts.some((heart) => {
